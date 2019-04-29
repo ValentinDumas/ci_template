257ea78d7dcae35c4f8de95234bd3f1dd9c4fc57
Valentin Dumas <valentin.dumas@viacesi.fr>
2019-04-29 17:04:02 +0200

    Lua dynamic linking

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 #include "mathematics/complex.hpp"
 
+#include "LuaScript.h"
+
 /// Main program function
 int main(int argc, char **argv)
 {
@@ -10,6 +12,16 @@ int main(int argc, char **argv)
     std::cout << "complex.getImag() = " << complex.getImag() << std::endl;
     std::cout << "complex.getReal() = " << complex.getReal() << std::endl;
     std::cout << "complex.abs() = " << complex.abs() << std::endl;
+
+    LuaScript script("assets\\scripts\\Player.lua");
+    float posX = script.get<float>("player.position.x");
+    float posY = script.get<float>("player.position.y");
+    std::string filename = script.get<std::string>("player.filename");
+    int hp = script.get<int>("player.HP");
+
+    std::cout<<"Position X = "<<posX<<", Y = "<<posY<<std::endl;
+    std::cout<<"Filename:"<<filename<<std::endl;
+    std::cout<<"HP:"<<hp<<std::endl;
 
     std::cout << "Setup status: SUCCESS !" << std::endl;
     // std::cin.get();
