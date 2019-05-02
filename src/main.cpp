4142ed14a778f409276807b60a0210d11fd2dfd8
Valentin Dumas <valentin.dumas@viacesi.fr>
2019-05-02 17:35:50 +0200

    Filesystem C lib files

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,16 @@
 /// Main program function
 int main(int argc, char **argv)
 {
-
-
-
     auto path = utils::filesystem::get_current_path();
     std::cout << "Current main path: " << path << std::endl;
 
-    utils::filesystem::list_directory_entries(path);
+    utils::filesystem::print_directory_entries(path);
 
     auto a = utils::filesystem::get_entry(path + "\\ci_template.exe");
 
-
+    std::cout << "External number's value: " << utils::filesystem::external_number << std::endl;
+    utils::filesystem::print_directory_entries("");
+    std::cout << "External number's value: "  << utils::filesystem::external_number << std::endl;
 
     auto t_index = getTypeIndex<int>();
 
@@ -39,6 +38,8 @@ int main(int argc, char **argv)
     std::cout<<"Position X = "<<posX<<", Y = "<<posY<<std::endl;
     std::cout<<"Filename:"<<filename<<std::endl;
     std::cout<<"HP:"<<hp<<std::endl;
+
+    utils::filesystem::get_tree(path);
 
     std::cout << "Setup status: SUCCESS !" << std::endl;
     // std::cin.get();
--- src/utils/FileSystem.h
+++ src/utils/FileSystem.h
@@ -12,15 +12,38 @@
 #include <cppfs/FileHandle.h>
 #include <cppfs/Tree.h>
 
+#include <vector>
+
+#if __cplusplus
+    #define EXTERN extern "C"
+#else
+    #define EXTERN
+#endif
+
 namespace utils {
     namespace filesystem {
+        std::string get_filename(const std::string& path);
+
+        bool set_working_path(const std::string& path);
+
+        void get_tree_filepaths(std::unique_ptr<cppfs::Tree>& file_tree);
+
+        // # TODO return list of list of ... of vectors (std::vector<std::string>...), representing the tree and containing all the item paths !
+        void get_tree(const std::string& path, bool include_hash = false);
+
         std::string get_current_path();
 
-        void list_directory_entries(const std::string &path);
+        std::vector<std::string> get_directory_entries(const std::string & path);
+
+        void print_directory_entries(const std::string &path);
 
         void list_recursive_directory_entries(const std::string &path);
 
         cppfs::FileHandle get_entry(const std::string &path);
+
+        EXTERN int external_number;
+
+        //EXTERN std::string current_working_path;
     }
 }
 
